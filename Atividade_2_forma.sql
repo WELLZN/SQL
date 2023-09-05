3230f424d64a34373ff5f12386502ce9d6bc40f8
wellzn <wellithuglife@gmail.com>
2023-09-05 15:16:07 -0300

    Atividade 2 forma normal

---
+++ Atividade_2_forma.sql
@@ -0,0 +1,33 @@
+create database Atividade_2_forma;
+use Ativivdade_2_forma;
+
+Parte 1
+
+A tabela não esta de acordo com a 2 forma normal, visto que ela ja não se encontra na 1 forma normal
+Visto que a tabela possui um atributo multivalorado, que é o endereço correto
+
+Além disso, ela possue o atributo Nome do Cliente que é parcialmente dependente da chave primária(ID do pedido)
+Assim o idela que a tabela pedidos fosse criada com apenas os campos: ID do Pedido e Produto do Pedido.Os demais campos seriam criados em outra tabela, no caso uma tablea Clientes, por exemplo.
+
+
+Parte 2
+
+A tabela esta na 2 Forma Normal ela esta de acordo com a 1 Forma Normal, e não possui nenhum campo está parcialmente ou toltalmente dependente de outro campo.
+
+Parte 3
+
+A tabela Funcionários não esta de acordo com a 2 Forma normal , pois tambem não está de acordo com a 1 Forma Normal , visto que o atributo endereço do Departamento é multivalorado.
+Além disso, o atributo Endereço do Departamento é parcialmente dependente do Campo Departamento.
+
+Parte 4
+
+A tabela não esta na 2 Forma Normal , pois o atributo Departamento do Professor é dependente do campo Professor.
+
+
+Parte 5
+
+A tabela não está na 2 Forma Normal , pois o Nome do Cliente e Cidade do Cliente são dependentes . O ideal seria a criação de outra tabela Clientes para armazenar essas informações.
+
+Parte 6
+
+A tabela não está na 2 Forma Normal , pois os campos Nome do Vendedor . Nome do Vendedor e Departamento do vendedor são pendentes de outros campos.
