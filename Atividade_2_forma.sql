9aadf640d9cb643b6ccd8705aa84db98ebaa3f43
wellzn <128332156+WELLZN@users.noreply.github.com>
2023-09-05 17:29:44 -0300

    Update Atividade_2_forma.sql

--- Atividade_2_forma.sql
+++ Atividade_2_forma.sql
@@ -4,7 +4,7 @@ use Ativivdade_2_forma;
 Parte 1
 
 A tabela não esta de acordo com a 2 forma normal, visto que ela ja não se encontra na 1 forma normal
-Visto que a tabela possui um atributo multivalorado, que é o endereço correto
+porque a tabela possui um atributo multivalorado, que é o endereço correto
 
 Além disso, ela possue o atributo Nome do Cliente que é parcialmente dependente da chave primária(ID do pedido)
 Assim o idela que a tabela pedidos fosse criada com apenas os campos: ID do Pedido e Produto do Pedido.Os demais campos seriam criados em outra tabela, no caso uma tablea Clientes, por exemplo.
@@ -16,13 +16,12 @@ A tabela esta na 2 Forma Normal ela esta de acordo com a 1 Forma Normal, e não 
 
 Parte 3
 
-A tabela Funcionários não esta de acordo com a 2 Forma normal , pois tambem não está de acordo com a 1 Forma Normal , visto que o atributo endereço do Departamento é multivalorado.
+A tabela Funcionários não esta de acordo com a 2 Forma normal , pois tambem não está de acordo com a 1 Forma Normal , poruqe o atributo endereço do Departamento é multivalorado.
 Além disso, o atributo Endereço do Departamento é parcialmente dependente do Campo Departamento.
 
 Parte 4
 
 A tabela não esta na 2 Forma Normal , pois o atributo Departamento do Professor é dependente do campo Professor.
-
 
 Parte 5
 
